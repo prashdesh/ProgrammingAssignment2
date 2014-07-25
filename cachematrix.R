6b22aa07eedd641570f2eb4258938ece63b20b6b
prashdesh <prashdesh@live.com>
2014-07-24 23:56:26 -0700

    Coursera exercise

--- cachematrix.R
+++ cachematrix.R
@@ -5,6 +5,33 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+## Creates a list of functions that
+
+## can cache the inverse of a matrix.
+
+    m <- NULL
+
+    set <- function(y) {
+
+        x <<- y
+
+        m <<- NULL
+
+    }
+
+    get <- function() x
+
+    setInverse <- function(inverse) m <<-inverse
+
+    getInverse <- function() m
+
+    list(set = set, get = get,
+
+         setInverse = setInverse,
+
+         getInverse = getInverse)
+
+
 }
 
 
@@ -12,4 +39,29 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+## Computes the inverse of the matrix returned
+
+## by makeCacheMatrix(), unless the inverse has
+
+## already been calculated, in which case
+
+## it retrieves it from the cache.
+
+    m <- x$getInverse()
+
+    if ( ! is.null(m)) {
+
+        print("getting cached data")
+
+        return(m)
+
+    }
+
+    m <- solve(x$get())
+
+    x$setInverse(m)
+
+   m
+
 }
